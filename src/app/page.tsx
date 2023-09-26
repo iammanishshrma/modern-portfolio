fcc14fbe3e8a9d5b218aebf4ccd60bf849da8f56
Manish Sharma <manish.sharma@sparxitsolutions.com>
2023-09-26 17:16:57 +0530

    added home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,60 @@
+import Image from "next/image";
+import myImg from "@/assets/images/me.jpg";
+
+import classes from "./page.module.scss";
+
 export default function Home() {
-    return <div></div>;
+    return (
+        <div className={classes.home}>
+            <div className={classes.image}>
+                {/* <Image src={myImg.src} fill alt="Manish Sharma" /> */}
+                <div></div>
+            </div>
+            <div className={classes.details}>
+                <h1>Manish Sharma</h1>
+                <p>
+                    I&apos;m a full time web developer and want to create
+                    everyone&apos;s web experience better.
+                </p>
+                <ul className={classes.icons}>
+                    <li className={classes.icon}>
+                        <a
+                            href="https://www.linkedin.com/in/iammanishshrma"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <i className={classes.linkedin}></i>
+                        </a>
+                    </li>
+                    <li className={classes.icon}>
+                        <a
+                            href="https://github.com/iammanishshrma"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <i className={classes.github}></i>
+                        </a>
+                    </li>
+                    <li className={classes.icon}>
+                        <a
+                            href="http://"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <i className={classes.fb}></i>
+                        </a>
+                    </li>
+                    <li className={classes.icon}>
+                        <a
+                            href="http://"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <i className={classes.x}></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
 }
